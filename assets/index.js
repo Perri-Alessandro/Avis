aa779643e95a60f6e1c58ad9ea1404f56ce82a72
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-11-11 02:53:10 +0100

    news pages drafs

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,18 @@
-document.addEventListener("scroll", function () {
+let scrolla = document.addEventListener("scroll", function () {
   const navbar = document.querySelector("nav");
   const icons = document.querySelector(".row.my-2");
   if (window.scrollY > icons.offsetHeight) {
     navbar.classList.add("fixed-top");
   } else {
     navbar.classList.remove("fixed-top");
+  }
+});
+
+const navLinks = document.querySelectorAll("header > nav div > div div a");
+navLinks.forEach((link) => {
+  if (link.href === window.location.href) {
+    link.classList.add("active");
+  } else {
+    link.classList.remove("active");
   }
 });
