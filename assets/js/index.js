81dd2ed8e41d6f3fc9c99a3cfcb5f65734048102
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-11-18 16:23:07 +0100

    "active-pagePath" name on navBar fixed for home. HeroText animation.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,36 @@
+let scriviHero = document.addEventListener("DOMContentLoaded", () => {
+  const topElement = document.getElementById("heroP1");
+  const bottomElement = document.getElementById("heroP2");
+
+  const preparaText = (element, text) => {
+    element.textContent = ""; // Svuota il testo esistente, lasciato solo per
+    text.split("").forEach((char) => {
+      const span = document.createElement("span");
+      span.textContent = char;
+      span.style.visibility = "hidden"; // Nasconde inizialmente ogni lettera
+      element.appendChild(span);
+    });
+  };
+
+  const digita = (element, text, index = 0) => {
+    const spans = element.querySelectorAll("span");
+    if (index < spans.length) {
+      spans[index].style.visibility = "visible";
+      setTimeout(() => digita(element, text, index + 1), 110);
+    }
+  };
+
+  preparaText(topElement, "Io Dono Sangue...");
+  preparaText(bottomElement, "Non So Per Chi, Ma So Perchè!");
+
+  digita(topElement, "Io Dono Sangue...", 0);
+
+  setTimeout(
+    () => digita(bottomElement, "Non So Per Chi, Ma So Perchè!", 0),
+    2100
+  );
+});
+
 let scrolla = document.addEventListener("scroll", function () {
   const navbar = document.querySelector("nav");
   const icons = document.querySelector(".row.my-2");
@@ -14,7 +47,12 @@ let scrolla = document.addEventListener("scroll", function () {
 
 const navLinks = document.querySelectorAll("header > nav div > div div a");
 navLinks.forEach((link) => {
-  if (link.href === window.location.href) {
+  const linkPath = new URL(link.href, window.location.origin).pathname; // assicura che linkPath contenga solo il percorso relativo (es. /assets/chi_siamo.html) senza dominio e protocollo.//
+  const currentPath = window.location.pathname; //Contiene il percorso della pagina corrente (es. /assets/chi_siamo.html o / per la radice).//
+  if (
+    linkPath === currentPath ||
+    (currentPath === "/" && linkPath.endsWith("/index.html"))
+  ) {
     link.classList.add("active");
   } else {
     link.classList.remove("active");
