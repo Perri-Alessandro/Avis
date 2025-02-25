ce0a212698f23a36c03680b1b80f8275495a53cf
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2025-02-25 03:21:01 +0100

    dynamic hero-section with different layout for mobile and deskop

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,25 @@
-let scriviHero = document.addEventListener("DOMContentLoaded", () => {
-  const topElement = document.getElementById("heroP1");
-  const bottomElement = document.getElementById("heroP2");
+const mobileHero = document.querySelector("#heroMobile");
+const desktopHero = document.querySelector("#heroDesktop");
+const mobileTopElement = document.querySelector("#heroMobile .heroP1");
+const mobileBottomElement = document.querySelector("#heroMobile .heroP2");
+const desktopTopElement = document.querySelector("#heroDesktop .heroP1");
+const desktopBottomElement = document.querySelector("#heroDesktop .heroP2");
+
+const updateHeroVisibility = () => {
+  const screenWidth = window.innerWidth;
+
+  let topElement, bottomElement;
+  if (screenWidth <= 850) {
+    mobileHero.style.display = "block";
+    desktopHero.style.display = "none";
+    topElement = mobileTopElement;
+    bottomElement = mobileBottomElement;
+  } else {
+    mobileHero.style.display = "none";
+    desktopHero.style.display = "block";
+    topElement = desktopTopElement;
+    bottomElement = desktopBottomElement;
+  }
 
   const preparaText = (element, text) => {
     element.textContent = ""; // Svuota il testo esistente, lasciato solo per spazio elemento in pagina
@@ -24,18 +43,22 @@ let scriviHero = document.addEventListener("DOMContentLoaded", () => {
   preparaText(bottomElement, "Non So Per Chi, Ma So Perchè!");
 
   setTimeout(() => digita(topElement, "Io Dono Sangue...", 0), 320);
-
   setTimeout(
     () => digita(bottomElement, "Non So Per Chi, Ma So Perchè!", 0),
     2400
   );
-});
+};
+
+window.addEventListener("load", updateHeroVisibility);
+window.addEventListener("resize", updateHeroVisibility);
+
+// ------------------------------------------------------------------------------------------------------------------------------------------
 
 let scrolla = document.addEventListener("scroll", function () {
   const navbar = document.querySelector("nav");
   const icons = document.querySelector(".row.my-2");
   const logoNav = document.querySelector(".avis_monte_icon");
-  const goccia = document.querySelector("#goccia");
+  // const goccia = document.querySelector("#goccia");
   if (window.scrollY > icons.offsetHeight) {
     navbar.classList.add("fixed-top");
     logoNav.classList.add("transitionnn");
@@ -48,7 +71,7 @@ let scrolla = document.addEventListener("scroll", function () {
 const navLinks = document.querySelectorAll("header > nav div > div div a");
 navLinks.forEach((link) => {
   const linkPath = new URL(link.href, window.location.origin).pathname; // assicura che linkPath contenga solo il percorso relativo (es. /assets/chi_siamo.html) senza dominio e protocollo.//
-  const currentPath = window.location.pathname; //Contiene il percorso della pagina corrente (es. /assets/chi_siamo.html o / per la radice).//
+  const currentPath = window.location.pathname; //Contiene il percorso della pagina corrente
   if (
     linkPath === currentPath ||
     (currentPath === "/" && linkPath.endsWith("/index.html"))
